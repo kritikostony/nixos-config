3c33540f7cc042a9a04ea4fa6b632576fefc1724
System administrator <kritikostony@gmail.com>
2025-11-16 23:52:52 +0200

    tests

--- flake.nix
+++ flake.nix
@@ -13,11 +13,11 @@
           modules = [ hostModule ];
         };
     in {
-    # Custom command that runs rebuild
-    packages.x86_64-linux.tony-nixos =
-      nixpkgs.legacyPackages.x86_64-linux.writeShellScriptBin "tony-nixos" ''
-        sudo nixos-rebuild switch --flake /etc/nixos-config
-      '';
+      # Custom command that runs rebuild
+      packages.x86_64-linux.tony-nixos =
+        nixpkgs.legacyPackages.x86_64-linux.writeShellScriptBin "tony-nixos" ''
+          sudo nixos-rebuild switch --flake /etc/nixos-config
+        '';
       nixosConfigurations = {
         tony-server = mkHost ./hosts/tony-server/default.nix;
         tony-backup-server = mkHost ./hosts/tony-backup-server/default.nix;
