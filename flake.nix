ee67da0ae5416be20ca42403c547d2a25e72187a
System administrator <kritikostony@gmail.com>
2025-11-17 00:03:56 +0200

    tests

--- flake.nix
+++ flake.nix
@@ -7,17 +7,29 @@
 
   outputs = { self, nixpkgs, ... }:
     let
-      mkHost = hostModule:
-        nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [ hostModule ];
-        };
+	mkHost = hostModule:
+	  nixpkgs.lib.nixosSystem {
+	    system = "x86_64-linux";
+	    modules = [
+	      hostModule
+	      # Inject overlay so pkgs.tony-nixos exists
+	      ({ pkgs, ... }: {
+	        nixpkgs.overlays = [ self.overlays.default ];
+	      })
+	    ];
+	  };
     in {
-      # Custom command that runs rebuild
+      # Custom rebuild command
       packages.x86_64-linux.tony-nixos =
         nixpkgs.legacyPackages.x86_64-linux.writeShellScriptBin "tony-nixos" ''
           sudo nixos-rebuild switch --flake /etc/nixos-config
         '';
+      # Overlay to expose the package as pkgs.tony-nixos
+      overlays = {
+        default = final: prev: {
+          tony-nixos = self.packages.${prev.system}.tony-nixos;
+        };
+      };
       nixosConfigurations = {
         tony-server = mkHost ./hosts/tony-server/default.nix;
         tony-backup-server = mkHost ./hosts/tony-backup-server/default.nix;
